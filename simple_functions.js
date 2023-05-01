f24b6c8e8a169f40b92ed62c0846b94a793e263d
paulette <paulette.yigo@gmail.com>
2023-05-01 11:40:40 -0400

    finished exercise 1

---
+++ simple_functions.js
@@ -0,0 +1,24 @@
+// functions to name my favorite things
+function favoriteThings(){
+    let favorite_things={
+        name: "Pau",
+        movie: "Home Alone",
+        musician: "Wizkid"
+    }
+    console.log(favorite_things);
+}
+favoriteThings();
+
+// name reasons why in the program
+function whyAmIHere(){
+    let reason="For a better future";
+    console.log(reason)
+}
+whyAmIHere();
+
+// favorite places
+function favoritePlace(){
+    let myFavoritePlace="I would love to visit Zanzibar one day. Its beaches are beautiful and resorts are affordable.";
+    console.log(myFavoritePlace)
+}
+favoritePlace();
